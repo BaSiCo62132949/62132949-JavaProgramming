b43a57d2c5e9a7511feeb5a3ad7796d6ae4bb71d
BaSiCo62132949 <co.bs.62cntt@ntu.edu.vn>
2025-02-18 15:07:03 +0700

    xong code

--- ViDuNhapXuat/src/ntu/basico/CTTinhTong.java
+++ ViDuNhapXuat/src/ntu/basico/CTTinhTong.java
@@ -1,10 +1,24 @@
 package ntu.basico;
 
+import java.util.Scanner;  
+
 public class CTTinhTong {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        System.out.println("CT TÍNH TỔNG\n");
+        
+        Scanner banPhim = new Scanner(System.in);
+        
+        System.out.print("Mời nhập a: ");
+        double a = banPhim.nextDouble();
+        
+        System.out.print("Mời nhập b: ");
+        double b = banPhim.nextDouble();
 
-	}
-
+        double tong = a + b;
+        
+        System.out.println("Tổng là: " + tong);
+        
+        banPhim.close();
+    }
 }
