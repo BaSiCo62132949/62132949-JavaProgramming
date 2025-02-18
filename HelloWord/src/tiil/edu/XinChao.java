626fa27e5d7d147e65fd21774591ef0ad98bc896
BaSiCo62132949 <co.bs.62cntt@ntu.edu.vn>
2025-02-18 14:45:02 +0700

    Cap nhat code

--- HelloWord/src/tiil/edu/XinChao.java
+++ HelloWord/src/tiil/edu/XinChao.java
@@ -14,6 +14,7 @@ public class XinChao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" CHAO MUNG DEN VOI LAP TRINH JAVA");
+		System.out.println(" HELLO WELCOME TO NHA TRANG UNIVERSITY");
 
 	}
 
