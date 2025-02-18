c7564923a0387ac52685d124a4848fd5b6093e71
BaSiCo62132949 <co.bs.62cntt@ntu.edu.vn>
2025-02-18 14:39:38 +0700

    Ví dụ đầu tiên

---
+++ HelloWord/src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" CHAO MUNG DEN VOI LAP TRINH JAVA");
+
+	}
+
+}
